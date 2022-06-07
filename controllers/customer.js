13f34f3e3bc58390b090467c45ecd7bd544fcc3d
Josiah <jwalutsachi4@gmail.com>
2022-06-07 21:59:50 +0300

    current user buys

--- controllers/customer.js
+++ controllers/customer.js
@@ -10,8 +10,8 @@ const db = require('../models/customer.js');
 
 const buyProduct = async (req, res) => {
 
-    const { product_id, quantity, unit_price, username } = req.body;
-
+    const { product_id, quantity, unit_price } = req.body;
+    const username = req.session.user;
     try {
         const check = await db.check_product(product_id);
         if (check[0].quantity === 0) {
